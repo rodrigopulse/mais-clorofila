37b5c4850f09a27881df615b05e7dfba653aeb5b
Rodrigo Godoy <contato@rodrigogodoy.net>
2020-12-18 16:48:43 -0300

    api com docker

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM node:14.15.2
+WORKDIR /home/node/app
+RUN npm i nodemon -g
--- api/src/index.js
+++ api/src/index.js
@@ -1,26 +1,33 @@
-import express from 'express'
-import routes from './routes'
-import dotenv from 'dotenv'
-import mongoose from 'mongoose'
+import express from "express";
+import routes from "./routes";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-dotenv.config()
+dotenv.config();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080;
 
 // mongoose
-const user = process.env.DB_USER
-const password = process.env.DB_PASSWORD
-const host = process.env.DB_HOST
-mongoose.connect(`mongodb+srv://${user}:${password}@${host}`, {
+const {
+  MONGO_USER,
+  MONGO_HOST,
+  MONGO_PASSWORD,
+  MONGO_PORT,
+  MONGO_DATA,
+} = process.env;
+const url = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATA}?authSource=admin`;
+mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true
-})
+  useCreateIndex: true,
+});
 
-app.use(routes)
+app.use(routes);
 
-app.listen(port, () => { console.log(`app listening at http://localhost:${port}`) })
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`);
+});
