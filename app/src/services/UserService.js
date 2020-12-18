cf3cafaddfb67a29b98c34aa1858d244d21b6ee0
Rodrigo Godoy <contato@rodrigogodoy.net>
2020-12-18 18:37:20 -0300

    Corrigidas as portas entre o app e a api

--- app/src/services/UserService.js
+++ app/src/services/UserService.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-import { API_URL } from '@env';
+// import { API_URL } from '@env';
 
 export const SignUpService = async (data) => {
   try {
     const response = await axios({
-      url: `${API_URL}/user/register`,
+      url: `http://192.168.1.105:8082/user/register`,
       data: data,
       method: 'POST',
     });
